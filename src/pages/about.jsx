f52c9b66f39875fae47ed79e1955f203149d6252
sachinkushwha <152092000+sachinkushwha@users.noreply.github.com>
2025-06-25 14:09:55 +0530

    Update about.jsx

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,7 +1,7 @@
 export const About = () => {
     return (
         <div className="pt-20 w-[95%] max-w-4xl mx-auto py-10 px-4">
-            <h1 className="text-4xl font-bold text-center text-blue-700 mb-6">About MotionCare</h1>
+            <h1 className="text-4xl font-bold text-center text-blue-700 mb-6">About drkalimphysio</h1>
             <p className="text-gray-700 text-lg leading-relaxed mb-4">
                 At <span className="font-semibold text-blue-600">Dr.kalimphysio Clinic</span>, we are committed to helping you move better, live stronger, and heal faster. With a team of certified and experienced physiotherapists, we provide advanced care tailored to each individual’s needs — whether you're recovering from an injury, surgery, or managing chronic pain.
             </p>
